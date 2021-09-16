b1d25224bb454435f9a8cb5caec8e02b83e32973
jdcloud-apigateway <jdcloud-api@jd.com>
2021-09-16 08:50:48 +0000

    jdcloud-sdk-java

--- cdn/src/main/java/com/jdcloud/sdk/service/cdn/client/CdnClient.java
+++ cdn/src/main/java/com/jdcloud/sdk/service/cdn/client/CdnClient.java
@@ -603,7 +603,7 @@ public class CdnClient extends JdcloudClient {
 
     public final static String ApiVersion = "v1";
     private final static String UserAgentPrefix = "JdcloudSdkJava";
-    public final static String ClientVersion = "1.2.3";
+    public final static String ClientVersion = "1.2.4";
     public final static String DefaultEndpoint = "cdn.jdcloud-api.com";
     public final static String ServiceName = "cdn";
     public final static String UserAgent = UserAgentPrefix + "/" + ClientVersion + " " + ServiceName + "/" + ApiVersion;
